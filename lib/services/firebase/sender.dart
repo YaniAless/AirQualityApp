a7187d593d6a1bfeb00eb592412919f5fe84f174
LukasBrass <lukas.brasseleur@gmail.com>
2020-07-05 23:44:00 +0200

    Update sender service to add humidity and use good type for each param

--- lib/services/firebase/sender.dart
+++ lib/services/firebase/sender.dart
@@ -4,11 +4,12 @@ class SenderService {
 
   //Fonction qui permet de persister les informations sur Firebase, exemple :
   //SenderService().sendData("UserPhoneTest",  34.0, 3500.0, 420.0);
-  void sendData(String username, double temperature, double co2, double tvoc) {
+  void sendData(String username, double temperature, int humidity, int co2, int tvoc) {
     DateTime dateNow = DateTime.now();
     final dbRef = FirebaseDatabase.instance.reference().child("Users/$username/${dateNow.year.toString()}/${dateNow.month.toString()}/${dateNow.day.toString()}/${dateNow.hour.toString()}/${dateNow.minute.toString()}");
     dbRef.set(<String, Object>{
       "temperature": temperature,
+      "humidity": humidity,
       "co2": co2,
       "tvoc": tvoc,
     }).then((r) => print("C'est envoyé ta mère"));
